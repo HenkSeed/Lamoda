fb8ad88d823604a6559b1348d1ad8b8fc55f38b3
HenkSeed <55748514+HenkSeed@users.noreply.github.com>
2021-07-07 00:10:01 +0700

    Day 01. Блокировка скролла

--- scripts/script.js
+++ scripts/script.js
@@ -15,17 +15,61 @@ headerCityButton.addEventListener('click', () => {
 	localStorage.setItem('lomoda-location', city);
 });
 
-// Модальное окно
+// БЛОКИРОВКА СКРОЛЛА
+
+// 1-й способ - при открытии модального окна происходит сдвиг фона
+// на ширину убираемого правого скроллбара
+
+const disableScroll = () => {
+	// document.body.style.overflow = 'hidden'; // 1-й способ
+
+	// window.innerWidth - ширина окна со скроллбаром
+	// document.body.offsetWidth - ширина окна без скроллбара
+	const widthScroll = window.innerWidth - document.body.offsetWidth; // ширина скроллбара
+
+	// Добавим (создадим и сохраним) новое свойство dbScrollY для body
+	// Оно "запомнит" значение проведенного скролла по оси Y
+	document.body.dbScrollY = window.scrollY;
+
+	// форма ${}, используемая ниже, называется интерполяция
+	// position: fixed; // применённое ниже,
+	// width: 100%;	// используется для
+	// height: 100vh;	// совместимости с iPhone
+
+	document.body.style.cssText = `
+	position: fixed; 
+	top: ${-window.scrollY}px;
+	left: 0;
+	width: 100%;
+	height: 100vh;
+	overflow: hidden;
+	padding-right: ${widthScroll}px;
+	`;
+};
+
+const enableScroll = () => {
+	// document.body.style.overflow = ''; // 1-й способ
+	document.body.style.cssText = '';
+
+	// исключает скролл в начало страницы при закрытии модального окна
+	window.scroll({
+		top: document.body.dbScrollY,
+	});
+};
+
+// МОДАЛЬНОЕ ОКНО
 
 const subheaderCart = document.querySelector('.subheader__cart');
 const cartOverlay = document.querySelector('.cart-overlay');
 
 const cartModalOpen = () => {
 	cartOverlay.classList.add('cart-overlay-open');
+	disableScroll(); // 1-й и 2-й способы
 };
 
 const cartModalClose = () => {
 	cartOverlay.classList.remove('cart-overlay-open');
+	enableScroll(); // 1-й и 2-й способы
 };
 
 subheaderCart.addEventListener('click', cartModalOpen);
